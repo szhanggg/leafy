2c754aecc85ade3bb43610e9f223e5573b1d3b3e
szhanggg <stevez9101@gmail.com>
2023-12-19 10:01:47 -0500

    first page additions

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Work Sans", "sans-serif"],
+      },
+      colors: {
+        primary: "#3FA929",
+      },
+    },
+  },
+  plugins: [],
+};
