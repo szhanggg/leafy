e899ed0dd125601ee60ebb42012f6418136a7810
szhanggg <stevez9101@gmail.com>
2024-01-06 03:11:05 -0500

    careers page

---
+++ .vercel/output/static/_astro/hoisted.q5XBFhxU.js
@@ -0,0 +1,1 @@
+window.addEventListener("scroll",()=>{const s=document.getElementById("navbar");window.scrollY>0?(s.classList.remove("bg-transparent"),s.classList.add("bg-white")):(s.classList.remove("bg-white"),s.classList.add("bg-transparent"))});
