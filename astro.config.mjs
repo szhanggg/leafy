14a160c0913abf3b7fc10fba4de68d8d25244046
szhanggg <stevez9101@gmail.com>
2024-03-14 12:22:18 -0400

    optimizations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import vercelStatic from "@astrojs/vercel/static";
-
 import tailwind from "@astrojs/tailwind";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), compress()],
   output: "static",
-  adapter: vercelStatic(),
+  adapter: vercelStatic()
 });
